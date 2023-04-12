200b9dc818eb53ebd9b18f1ed30aa8bf35deea3c
tykman <nizom.ra@inbox.ru>
2023-04-12 06:11:56 +0500

    Дз. Variable 2.

---
+++ src/Main.java
@@ -0,0 +1,252 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("variable 2");
+
+
+        byte apples = 123;
+        System.out.println("Значение переменной apples с типом byte равно 123");
+        short monthApril1 = 30;
+        System.out.println("Значение переменной monthApril1 с типом  short равно 30");
+        int dollarsPrize = 2147483647;
+        System.out.println("Значение переменной dollarsPrize с типом int равно 2147483647");
+        int universe = 100000000 + 8;
+        System.out.println("Значение переменной universe с типом int равно 100000000 + 8");
+        double percent = 1.7;
+        System.out.println("Значение переменной percent с типом double равно 1.7");
+
+        double gram =27.12;
+        System.out.println(gram);
+        double kiloMeters = 2.786;
+        System.out.println(kiloMeters);
+        int inch = 569;
+        System.out.println(inch);
+        short displacementOf = 27897;
+        System.out.println(displacementOf);
+        byte honor = 67;
+        System.out.println(honor);
+        double cubicMeters = 98.7678965549;
+        System.out.println(cubicMeters);
+
+        short a = 23;
+        short b = 27;
+        short c = 30;
+        int total = a + b + c;
+        System.out.println(total);
+
+        int total1 = 80;
+        int paperSheets = 480;
+        long junior = 480 / 80;
+        System.out.println("На каждого ученика" + junior + "листов бумаги");
+
+        byte a1 = 16;
+        byte b1 = 2;
+        byte c1 = 16 / 2;
+        System.out.println(c1);
+
+        byte min = 20;
+        byte bottle = 8;
+        int bottles = (min * bottle);
+        System.out.println("За" + min + "машина произвела" + bottles + "штук бутылок.");
+
+        byte oneDay = 24;
+        byte minHour = 60;
+        int minTotal = (oneDay * minHour);
+        System.out.println(minTotal);
+
+        byte bot = 8;
+        short minTot = 1440;
+        int botMin = (bot * minTot);
+        System.out.println("За" + min + "машина произвела" + botMin + "штук бутылок.");
+
+        byte threeDay = 3;
+        short minTotal2 = 1440;
+        int dayThree = (threeDay * minTotal2);
+        System.out.println(dayThree);
+
+        byte bottle3 = 8;
+        int minTotal3 = 4320;
+        long threeDay3 = (bottle3 * minTotal3);
+        System.out.println("За" + bottle3 + "машина произвела" + threeDay3 + "штук бутылок.");
+
+        byte monthDay = 30;
+        int minTotal4 = 1400;
+        int month = (monthDay * minTotal4);
+        System.out.println(month);
+
+        byte bottle4 = 8;
+        int totalMin4 = 4200;
+        long totalBottle = (bottle4 * totalMin4);
+        System.out.println("За" + totalMin4 + "машина произвела" + totalBottle + "штук бутылок.");
+
+        byte white = 2;
+        byte brawn = 4;
+        int paint = (white + brawn);
+        System.out.println(paint);
+
+        byte totalOf = 6;
+        byte paintOf = 120;
+        int paintOf1 = (paintOf / totalOf);
+        System.out.println(paintOf1);
+
+        byte classRoom = 20;
+        byte white1 = 2;
+        int paintWhite = (classRoom * white1);
+        System.out.println(paintWhite);
+
+        byte classRoom2 = 20;
+        byte brawn1 = 4;
+        int paintBrawn = ( classRoom2 * brawn1);
+        System.out.println(paintBrawn);
+
+        byte roomClass = 20;
+        byte whitePaint = 40;
+        byte brawnPaint = 80;
+        System.out.println("В школе, где" + roomClass + "классов, нужно" + whitePaint + "банок белой краски" + brawnPaint + "банок коричневой краски.");
+
+        byte bananas = 5;
+        byte bananasGram = 80;
+        int gramBananas = (bananas * bananasGram);
+        System.out.println(gramBananas);
+
+        byte milk = 2;
+        byte milkGr = 105;
+        int gramMilk = (milk * milkGr);
+        System.out.println(gramMilk);
+
+        byte iseCream = 4;
+        byte iseGr = 100;
+        int gramIse = (iseCream * iseGr);
+        System.out.println(gramIse);
+
+        byte egg = 4;
+        byte eggGr = 70;
+        int gramEgg = (egg * eggGr);
+        System.out.println(gramEgg);
+
+        short grBan = 400;
+        short grMilk = 210;
+        short grIse = 200;
+        short grEgg = 280;
+        int breakfast = (grBan + grMilk + grIse + grEgg);
+        System.out.println(breakfast + "гр!");
+
+        double banGram = 0.40;
+        double milkGram = 0.21;
+        double iseGram = 0.20;
+        double eggGram = 0.28;
+        double jockBreak = (banGram + milkGram + iseGram + eggGram);
+        System.out.println(jockBreak + "кг!");
+
+        double weightMan = 7.000;
+        float grDay = (float)  0.250F;
+        byte days = (byte) (weightMan / grDay);
+        System.out.println(days + "дней.");
+
+        double manWeight = 7.000;
+        float dayGr = 0.500F;
+        byte dayOf = (byte) (manWeight / dayGr);
+        System.out.println(dayOf + "дней.");
+
+        long mariya = 67760;
+        byte monthM = 12;
+        long yearM = (mariya * monthM);
+        System.out.println(yearM + "рублей на текущий год.");
+
+        long mari = 67760;
+        byte percentM = 10;
+        short mariyaMonth = (short) (mari / percentM);
+        System.out.println(mariyaMonth + "рублей, прибавка 10% в мес.");
+
+        short mariMa = 6776;
+        byte monthMa = 12;
+        int yearMa = (mariMa * monthMa);
+        System.out.println(yearMa + "рублей, общая сумма прибавки.");
+
+        int yearMar = 81312;
+        short monthMar = 12;
+        int yearMari = (yearMar * monthMar);
+        System.out.println(yearMari + "нынешний годовой доход.");
+
+        int mariYear = 975744;
+        int mariMonth = 813120;
+        int yearMari1 = (mariYear - mariMonth);
+        System.out.println(yearMari1 + "разница между годовым доходом до и после повышения.");
+
+        int mariyaMonth1 = 67760;
+        long mariya1 = 6776;
+        long monthMariya1 = (mariyaMonth1 + mariya1);
+        System.out.println(monthMariya1 + "повышенная зарплата в мес.");
+
+        int monthMariya2 = 74536;
+        int mariya2 = 81312;
+        System.out.println("Маша теперь получает" + monthMariya2 + "рублей.Годовой доход вырос" + mariya2 + "рублей.");
+
+        long den = 83690;
+        byte monthD = 12;
+        long yearD = (den * monthD);
+        System.out.println(yearD + "рублей на текущий год.");
+
+        long deni = 83690;
+        byte percentD = 10;
+        short deniMonth = (short) ( deni / percentD);
+        System.out.println(deniMonth + "рублей, прибавка 10% в мес.");
+
+        short deniMany = 8369;
+        byte monthDen = 12;
+        int yearDen = (deniMany * monthDen);
+        System.out.println(yearDen + "рублей, общая сумма прибавки.");
+
+        int yearDe = 100428;
+        short monthDe = 12;
+        int yearDeni = (yearDe * monthDe);
+        System.out.println(yearDeni + "нынешний годовой доход с прибавкой.");
+
+        int deniYear = 1205136;
+        int deniMonth1 = 1004280;
+        int yearDenis = (deniYear - deniMonth1);
+        System.out.println(yearDenis + "разница между между годовым доходом до и после повышения.");
+
+        int denisMonth2 = 83690;
+        long denis = 8369;
+        long monthDenis1 = (denisMonth2 + denis);
+        System.out.println(monthDenis1 + "повышенная з/п в мес.");
+
+        int monthDenis2 = 92059;
+        int denis1 = 100428;
+        System.out.println("Денис теперь получает" + monthDenis2 + "рублейюГодовой доход вырос" + denis1 +"рублей");
+
+        long cristen = 76230;
+        byte monthC = 12;
+        long yearsC = (cristen * monthC);
+        System.out.println(yearsC + "рублей на текущий год.");
+
+        long cristi = 76230;
+        byte percentC = 10;
+        short crisMonth = (short) (cristi / percentC);
+        System.out.println(crisMonth + "рублей, прибавка 10 % в мес");
+
+        short crisMany = 7623;
+        byte monthCris = 12;
+        int yearCris = (crisMany * monthCris);
+        System.out.println(yearCris + "рублей, общая сумма прибавки.");
+
+        int yearCristi = 91476;
+        short monthCri = 12;
+        int yearCristy = (yearCristi * monthCri);
+        System.out.println(yearCristy + "нынешний годовой доход с прибавкой.");
+
+        int cristiYear = 1097712;
+        int cristiMonth = 914760;
+        int yearCristin = (cristiYear - cristiMonth);
+        System.out.println(yearCristin + "разница между годовым доходом до и после повышения.");
+
+        int cristinMonth2 = 76230;
+        long cristine = 7623;
+        long monthCristin1 = (cristinMonth2 + cristine);
+        System.out.println(monthCristin1 + "повышенная з/п в мес.");
+
+        int monthCristin2 = 83853;
+        int cristin1 = 914760;
+        System.out.println("Кристина теперь получает" + monthCristin2 + " рублейюГодовой доход вырос" + cristin1 + "рублей.");
+    }
+}
